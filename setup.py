5b3d6704fea677253e5ed75c194c4119146e333f
naustica <nick.haupka@outlook.de>
2022-04-02 00:09:17 +0200

    update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(dir, 'README.md'), encoding='utf-8') as file:
 
 
 setup(name='bq_loader',
-      version='0.1.1',
+      version='0.1.2',
       description='',
       long_description=long_description,
       long_description_content_type='text/markdown',
