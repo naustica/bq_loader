f3b4c10dd5aff207169249a766667732443c6194
naustica <nick.haupka@outlook.de>
2022-04-02 16:29:20 +0200

    update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(dir, 'README.md'), encoding='utf-8') as file:
 
 
 setup(name='bq_loader',
-      version='0.1.3',
+      version='0.1.4',
       description='Interactive Command Line Interface for Google BigQuery',
       long_description=long_description,
       long_description_content_type='text/markdown',
